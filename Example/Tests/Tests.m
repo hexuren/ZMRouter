6f2f9cf98f7dfeb5c22c9317918427eaed4d26fb
Zhimi <529455009@qq.com>
2020-05-11 16:33:35 +0800

    Initial commit

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,35 @@
+//
+//  ZMRouterTests.m
+//  ZMRouterTests
+//
+//  Created by acct<blob>=0xE4BD95E8AEB8E4BABA on 05/11/2020.
+//  Copyright (c) 2020 acct<blob>=0xE4BD95E8AEB8E4BABA. All rights reserved.
+//
+
+@import XCTest;
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
+
---
+++ Example/ZMRouter/ZMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZMAppDelegate.h
+//  ZMRouter
+//
+//  Created by acct<blob>=0xE4BD95E8AEB8E4BABA on 05/11/2020.
+//  Copyright (c) 2020 acct<blob>=0xE4BD95E8AEB8E4BABA. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
